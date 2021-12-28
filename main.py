3054ec8491aa44b2c63a49ab19a9b8344c02eb74
Tim GF63 <tim@shatzir.com>
2021-12-28 23:19:33 +0900

    2812

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 answer = int(input("сколько вишен вы хотите съесть?"))
-if answer == 1 or answer == 21:
-    print(answer,"вишню")
-elif answer > 1 and answer < 5:
-    print(answer, "вишни")
-elif answer > 4 and answer < 21:
+if answer>=11 and answer<=14 or answer%10==0 or answer%10>=5 and answer%10<=9:
     print(answer, "вишен")
+elif answer%10==1:
+    print(answer,"вишню")
 else:
-    print("эй! это годовой запас вишни")
+    print(answer, "вишни")
+
+
+
